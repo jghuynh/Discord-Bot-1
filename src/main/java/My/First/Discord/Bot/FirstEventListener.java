82380e41765cf9410d63b7cd208bfb4bb75b6dfa
jghuynh <jghuynh@dons.usfca.edu>
2020-03-24 18:22:07 -0700

    Added 2 more events and a file

--- src/main/java/My/First/Discord/Bot/FirstEventListener.java
+++ src/main/java/My/First/Discord/Bot/FirstEventListener.java
@@ -20,14 +20,24 @@ public class FirstEventListener extends ListenerAdapter {
 //	ArrayList<Integer> list = new ArrayList<Integer>();
 	public FirstEventListener() {
 		verbs = new ArrayList<String>();
+		/*
+		 * getfile("GiangVerbs")
+		 * Read file
+		 * for every line in file
+		 * add that line to verbs
+		 */
 		verbs.add("farts explosively");
 		verbs.add("slaps the shit out of Jake");
 		verbs.add("sings melodiously");
 		verbs.add("raises cats");
 		verbs.add("explodes spectacularly");
+		verbs.add("bitch-slaps Andrew");
 		
 	}
 	
+	/**
+	 * Takes a message from the channel and responds
+	 */
 	@Override
 	public void onMessageReceived(MessageReceivedEvent event) {
 		/**
@@ -35,7 +45,8 @@ public class FirstEventListener extends ListenerAdapter {
 		 * Discord says "world!"
 		 */
 		MessageChannel channel = event.getChannel();
-		if (event.getMessage().getContentRaw().toLowerCase().equals("hello")) {
+		String receivedMessage = event.getMessage().getContentRaw().toLowerCase().trim(); 
+		if (receivedMessage.equals("hello")) {
 			//Send the message "world" in the same place the word "hello" was mentioned
 			channel.sendMessage("world!").queue();
 //			channel.sendMessage("world!");
@@ -47,11 +58,11 @@ public class FirstEventListener extends ListenerAdapter {
 		 * send that message (don't forget .queue()!)
 		 */
 		
-		else if (event.getMessage().getContentRaw().toLowerCase().equals("Giang")
-				|| event.getMessage().getContentRaw().toLowerCase().equals("Gianna")) {
-			String Giang = event.getMessage().getContentRaw().toLowerCase();
+		else if (receivedMessage.equals("giang")
+				|| receivedMessage.equals("gianna")) {
 			int index = (int) (Math.random() *verbs.size());
-			String message = Giang + verbs.get(index);
+			String message = receivedMessage + " " + verbs.get(index);
+			System.out.println("my message, " + message);
 			channel.sendMessage(message).queue();
 		}
 	}
